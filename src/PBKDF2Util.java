f294a1b182b03795c5129127ad49da199d6ff6d6
Felipe <lippee@gmail.com>
2019-04-27 16:28:39 -0300

    Passo 3 de Alice (com problemas de biblioteca)

--- src/PBKDF2Util.java
+++ src/PBKDF2Util.java
@@ -2,71 +2,44 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.KeyStore;
 import org.apache.commons.codec.binary.Hex;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Scanner;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
-//import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-/**
- * User: Carla
- * Date: 1 de setembro de 2015
- */
 
 public class PBKDF2Util {
     
-    /**
-     * Gerar chave derivada da senha
-     * @param key
-     * @param salt
-     * @param iterations
-     * @return
-     */
-    public static String generateDerivedKey(String password, String salt, Integer iterations) {
-        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), iterations, 128);
-        SecretKeyFactory pbkdf2 = null;
-        String derivedPass = null;
-        try {
-            pbkdf2 = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-            SecretKey sk = pbkdf2.generateSecret(spec);
-            derivedPass = Hex.encodeHexString(sk.getEncoded());
-            
-            storeSecretKey("meukeystore.bcfks", "password".toCharArray(), "pbkdf2", password.toCharArray(), sk);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return derivedPass;
-    }
-    
-    //Tenho PBEKeySpec e SecretKey 
-    
-    /*Usado para gerar o salt  */
-    public String getSalt() throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        //SecureRandom sr = new SecureRandom();
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return Hex.encodeHexString(salt);
-    }
+    private Cipher cipher;
 
-    public static void main(String args[]) throws NoSuchAlgorithmException {
+      public static void main(String args[]) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, GeneralSecurityException, IOException {
         
-        // Install Provider FIPS
+        //Adiciona Provider
         Security.addProvider(new BouncyCastleFipsProvider());
         
+        //Instancia objetos Pessoa
         Pessoa alice = new Pessoa(0L, "Alice");
         Pessoa bob = new Pessoa(1L, "Bob");
              
+        //Declara objetos úteis 
         PBKDF2Util obj = new PBKDF2Util();        
-        
-        String senha;
+        String senhaChave;
+        String senhaArmazenamentoKeyStore;
         String opcao;
         String salt;
         int it = 10000;
@@ -86,35 +59,65 @@ public class PBKDF2Util {
             System.out.println("Opção inválida!");
         }
         
-        //Alice:
-        
+        //Alice:     
         if (umaPessoa != null && umaPessoa.getNome().equals("Alice")) {
-            System.out.println("Digite a senha: ");
-            senha = input.nextLine(); //senha = "123456789";
+            System.out.println("Digite a senha para sua chave: ");
+            senhaChave = input.nextLine();
+            
+            System.out.println("Digite a senha para armazenamento sua chave: ");
+            senhaArmazenamentoKeyStore = input.nextLine();
+            
+            //Criando chave derivada
             salt = obj.getSalt();
-
-            System.out.println("Senha original = " + senha);
-            System.out.println("IV gerado = " + salt);
-            System.out.println("Numero de iteracoes = " + it);
-
-            String chaveDerivada = generateDerivedKey(senha, salt, it);
-
-            System.out.println("Chave derivada da senha = " + chaveDerivada );
-        }
+            PBEKeySpec spec = new PBEKeySpec(senhaChave.toCharArray(), salt.getBytes(), it, 128);
+                    
+            SecretKeyFactory pbkdf2 = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            SecretKey sk = pbkdf2.generateSecret(spec);
+            String chaveDerivada = Hex.encodeHexString(sk.getEncoded());
+            System.out.println("Chave derivada da senha gerada = " + chaveDerivada );
+            
+            //Criando IV
+            byte iv[] = new byte[16];
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            random.nextBytes(iv);
+            System.out.println("IV gerado = " + Utils.toHex(iv));
+            
+            storeSecretKey("meukeystore.bcfks", senhaArmazenamentoKeyStore.toCharArray(), senhaChave.toCharArray(), sk, iv);
+       
+            String mensagem1 = "Olá Bob!!! Quer conversar comigo?";
+            String mensagemCifrada = obj.cifraMensagem(mensagem1, sk, iv);
+            
+            System.out.println("Mensagem original: " + mensagem1);
+            System.out.println("Mensagem cifrada: " + mensagemCifrada);
+        }   
+     }
         
-        
+    /*Usado para gerar o salt  */
+    public String getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        //SecureRandom sr = new SecureRandom();
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return Hex.encodeHexString(salt);
+    }
+   
+    public String cifraMensagem(String mensagem, SecretKey key, byte[] iv) throws NoSuchAlgorithmException, NoSuchPaddingException, NoSuchProviderException, InvalidKeyException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
+       Security.addProvider(new BouncyCastleFipsProvider());
+       cipher = Cipher.getInstance("AES/GCM/NoPadding", "BCFIPS");
+       cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+       byte[] enc = cipher.doFinal(mensagem.getBytes());
+       String mensagemCifrada = Hex.encodeHexString(enc);
+       return mensagemCifrada;
     }
     
-    public static void storeSecretKey(String storeFilename, char[] storePassword, String alias, char[] keyPass, SecretKey secretKey)
-            throws GeneralSecurityException, IOException {
-        KeyStore keyStore = KeyStore.getInstance("BCFKS", "BCFIPS");
-        
-        keyStore.load(new FileInputStream(storeFilename), storePassword);
-        //keyStore.load(null, null);
+    public static void storeSecretKey(String storeFilename, char[] storePassword, char[] keyPass, SecretKey secretKey, byte[] iv) throws GeneralSecurityException, IOException {
+        KeyStore keyStore = KeyStore.getInstance("JCEKS");        
+        keyStore.load(null, null); //Cria do zero o KeyStore
+        keyStore.store(new FileOutputStream("meukeystore.bcfks"), storePassword); //Salva o KeyStore com a senha passada pelo usuário
 
-        keyStore.setKeyEntry(alias, secretKey, keyPass, null);
-        keyStore.store(new FileOutputStream(storeFilename), storePassword);
+        keyStore.load(new FileInputStream("meukeystore.bcfks"), storePassword); //Carrega o KeyStore salvo
+        keyStore.setKeyEntry("pbkdf2", secretKey, keyPass, null);
+        keyStore.setKeyEntry("iv", iv, null);
+        keyStore.store(new FileOutputStream("meukeystore.bcfks"), storePassword);
     }
-
-
 }
